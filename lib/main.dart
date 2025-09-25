5258b93cf00dc3fc052dbe36857cb025194235fc
yeomjungwoo <yeomi0206@gmail.com>
2025-09-26 01:10:43 +0900

    fix: change describution text on body

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
+// flutter에서 화면에 보이는 모든 것 = 위젯
+// 버튼, 텍스트, 화면 구조, margin, padding 전부 다 -> 위젯 (html에서 태그로 화면을 구성하는 것과 비슷한 논리)
+// Material Design 위젯 사용을 위한 import
 import 'package:flutter/material.dart';
 
+
+  // 앱의 시작점(entry point)
 void main() {
-  runApp(const MyApp());
+  // runApp: 루트 위젯을 화면에 붙여주는 역할
+  runApp(const MyApp()); 
 }
 
+// MyApp = 우리가 만든 앱을 감싸고 있는 위젯 = 루트 위젯
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // 루트 위젯에서는 MaterialApp을 리턴
+    // Material App = 
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -30,7 +39,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: '나의 첫 Flutter 페이지'),
+      home: const MyHomePage(title: '나의 첫 Flutter 페이지'), // 앱이 켜지면 가장 먼저 보여지는 부분
     );
   }
 }
@@ -51,6 +60,7 @@ class MyHomePage extends StatefulWidget {
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
+  // _MyHomePageState에서 실제 로직 + 상태 관리
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -104,7 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('버튼을 몇 번 눌렀을까용'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
