3f720de45f60ce538f415e2fa0a045e06310a7b8
yeomjungwoo <yeomi0206@gmail.com>
2025-11-09 01:34:52 +0900

    Feat: Add firebase connection in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'screens/todo_home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
